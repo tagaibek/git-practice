a2ca4e7d6815035f640731139e99ed234af27dce
Adyl <Adyl>
2017-10-14 22:20:48 +0600

    dev Branch

--- src/main/practice1014/Bootstrap.java
+++ src/main/practice1014/Bootstrap.java
@@ -3,5 +3,6 @@ package practice1014;
 public class Bootstrap {
     public static void main(String[] args) {
         System.out.println("Hello Git World!");
+        System.out.println("printing 1");
     }
 }
